0efb4eb247bbfb74735a5b4aab62c2c8d025d6d8
sujinzhou <sujz12345@163.com>
2016-08-22 11:34:36 +0800

    3 easy problem

--- src/com/exercise/Solution_01.java
+++ src/com/exercise/Solution_01.java
@@ -1,12 +1,25 @@
 package com.exercise;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Solution_01 {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		Solution_01 s= new Solution_01();
+//		int[] nums1 =new int[]{1,2};
+//		int[] nums2 =new int[]{1,1};
+//		Utils.printArr(s.intersect(nums1, nums2));
 		
+		String str1 = "1011";
+		String str2 = "101";
+		
+		System.out.println(s.addBinary2(str1, str2));
 	}
 	
 	/** 350 Easy
@@ -17,7 +30,32 @@ public class Solution_01 {
 	 * @return
 	 */
 	public int[] intersect(int[] nums1, int[] nums2) {
-        return null;
+        if(nums1.length == 0 || nums2.length == 0){
+        	return new int[0];
+        }
+		Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        List<Integer> list = new ArrayList<Integer>();
+        int i = 0;
+        int j = 0;
+        while(i < nums1.length && j < nums2.length){
+        	int a = nums1[i];
+        	int b = nums2[j];
+        	if(a < b){
+        		i++;
+        	}else if(a > b){
+        		j++;
+        	}else{
+        		list.add(a);
+        		i++;
+        		j++;
+        	}
+        }
+        int[] arr = new int[list.size()];
+        for (int k = 0; k < arr.length; k++) {
+			arr[k] = list.get(k);
+		}
+        return arr;
     }
 	/** 13 Easy
 	 * Given a roman numeral, convert it to an integer.
@@ -40,8 +78,91 @@ public class Solution_01 {
 	 * @return
 	 */
 	public String addBinary(String a, String b) {
-        return null;
+		String ret = "";
+        int length = Math.max(a.length(), b.length());
+        
+        char[] carryArr = new char[length + 1];//进位器
+        int pa = a.length();
+        int pb = b.length();
+		for(int i = 0;i <Math.abs(pa - pb) ; ++i){
+        	if(a.length() > b.length()){
+        		b = "0" + b;
+        	}else{
+        		a = "0" + a;
+        	}
+        }
+		for(int i = 0; i< carryArr.length; ++i){
+			carryArr[i] = '0';
+		}
+        for(int i = length - 1; i >= 0; --i){
+        	char str1 = a.charAt(i);
+        	char str2 = b.charAt(i);
+        	char carry = carryArr[i+1];
+        	if(str1 == '1' && str2 == '1'){
+        		if(carry == '0'){
+        			ret = "0" + ret;
+        		}else{
+        			ret = "1" + ret;
+        		}
+        		carryArr[i] = '1';
+        	}else if(str1 == '0' && str2 == '0'){
+        		if(carry == '0'){
+        			ret = "0" + ret;
+        		}else{
+        			ret = "1" + ret;
+        		}
+        	}else{
+        		if(carry == '0'){
+        			ret = "1" + ret;
+        		}else{
+        			ret = "0" + ret;
+        			carryArr[i] = '1';
+        		}
+        	}
+        }
+        if(carryArr[0] == '1'){
+        	ret = "1" + ret;
+        }
+		return ret;
     }
+	
+	public String addBinary2(String a, String b) {
+		if(a == null || a ==""){
+			return b;
+		}
+		if(b == null || b ==""){
+			return a;
+		}
+		StringBuilder ret = new StringBuilder();
+		int la = a.length();
+		int lb = b.length();
+		int length = Math.max(la, lb);
+		int flag = 0;
+		while(length > 0){
+			int ia = 0,ib = 0;
+			if(la > 0){
+				ia = a.charAt(la-1) == '0' ? 0 : 1;
+				la--;
+			}
+			if(lb > 0){
+				ib = b.charAt(lb-1) == '0' ? 0 : 1;
+				lb--;
+			}
+			int r = ia + ib + flag;
+			if(r >= 2){
+				flag = 1;
+				r -= 2;
+			}else{
+				flag = 0;
+			}
+			ret.insert(0,r);
+			length--;
+		}
+		if(flag == 1){
+			ret.insert(0, 1);
+		}
+		return ret.toString();
+	}
 	/** 66. Plus One(Easy)
 	 * Given a non-negative number represented as an array of digits, plus one to the number.
 	 * The digits are stored such that the most significant digit is at the head of the list.
---
+++ src/com/exercise/Utils.java
@@ -0,0 +1,18 @@
+package com.exercise;
+
+public class Utils {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+	
+	public static void printArr(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+
+}
