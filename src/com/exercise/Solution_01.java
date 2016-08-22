06252e2a4c2217de02242ed02971c2fdbe87aab0
sujinzhou <sujz12345@163.com>
2016-08-22 09:42:23 +0800

    add exercise_01

--- src/com/exercise/Solution_01.java
+++ src/com/exercise/Solution_01.java
@@ -6,8 +6,127 @@ public class Solution_01 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+	}
+	
+	/** 350 Easy
+	 * Given two arrays, write a function to compute their intersection.
+	 * Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2, 2].
+	 * @param nums1
+	 * @param nums2
+	 * @return
+	 */
+	public int[] intersect(int[] nums1, int[] nums2) {
+        return null;
+    }
+	/** 13 Easy
+	 * Given a roman numeral, convert it to an integer.
+	 * Input is guaranteed to be within the range from 1 to 3999
+	 * @param s
+	 * @return
+	 */
+	public int romanToInt(String s) {
+        return 0;
+    }
+	
+	/** 67. Add Binary(Easy)
+	 * Given two binary strings, return their sum (also a binary string).
+	 * For example,
+	 * a = "11"
+	 * b = "1"
+	 * Return "100".
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public String addBinary(String a, String b) {
+        return null;
+    }
+	/** 66. Plus One(Easy)
+	 * Given a non-negative number represented as an array of digits, plus one to the number.
+	 * The digits are stored such that the most significant digit is at the head of the list.
+	 * 
+	 * @param digits
+	 * @return
+	 */
+	public int[] plusOne(int[] digits) {
+        return new int[0];
+    }
+	/** 7. Reverse Integer(Easy)
+	 * Reverse digits of an integer.
+		Example1: x = 123, return 321
+		Example2: x = -123, return -321
+	 * @param x
+	 * @return
+	 */
+	public int reverse(int x) {
+        return 0;
+    }
+	
+    /** 8. String to Integer (atoi)(Easy)
+     * Implement atoi to convert a string to an integer.
 
+	Hint: Carefully consider all possible input cases. If you want a challenge, please do not see below and ask yourself what are the possible input cases.
+
+	Notes: It is intended for this problem to be specified vaguely (ie, no given input specs). You are responsible to gather all the input requirements up front.
+     * @param str
+     * @return
+     */
+	public int myAtoi(String str) {
+        return 0;
+    }
+    
+	/** 12 Medium
+	 * Given an integer, convert it to a roman numeral.
+	 * Input is guaranteed to be within the range from 1 to 3999.
+	 * @param num
+	 * @return
+	 */
+	public String intToRoman(int num) {
+        return null;
+    }
+	/** 2 Medium
+	 * You are given two linked lists representing two non-negative numbers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+	 * @param l1
+	 * @param l2
+	 * @return
+	 */
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return null;
+    }
+	/**
+	 * 43. Multiply Strings(Medium)
+	 * Given two numbers represented as strings, return multiplication of the numbers as a string.
+		Note:
+		The numbers can be arbitrarily large and are non-negative.
+		Converting the input string to integer is NOT allowed.
+		You should NOT use internal library such as BigInteger.
+	 * @param num1
+	 * @param num2
+	 * @return
+	 */
+	public String multiply(String num1, String num2) {
+        return null;
+    }
+	/**4. Median of Two Sorted Arrays(hard)
+	 * 返回两个数组的中位数
+	 * There are two sorted arrays nums1 and nums2 of size m and n respectively.
+		Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+	 * @param nums1
+	 * @param nums2
+	 * @return
+	 */
+	public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        return 0D;
+    }
+	
+	
+	
+	
+	class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
 	}
 
 }
